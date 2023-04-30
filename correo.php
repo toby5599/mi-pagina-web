d790b7ae239147afa03880e5ba6122531fd5030a
toby5599 <contacto.tobiasbenitez@gmail.com>
2023-04-30 04:17:14 -0300

    primer commit

---
+++ correo.php
@@ -0,0 +1,24 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre = $_POST["nombre"];
+    $email = $_POST["email"];
+    $numero = $_POST["numero"];
+    $mensaje = $_POST["mensaje"];
+
+}
+
+$destinatario = "tobiasdoyelbenitez@gmail.com";
+$asunto = "Nuevo mensaje de mi pagina web";
+$cuerpo = "Nombre: $nombre\nEmail: $email\nNumero: $numero\nMensaje:\n$mensaje";
+
+
+if (@mail($destinatario, $asunto, $cuerpo)) {
+    echo "gracias por contactarnos, $nombre. Tu mensaje ha sido enviado.";
+}
+
+else {
+    echo "Lo siento, hubo un problema al enviar tu mensaje";
+}
+
+?>
---
+++ java.js
@@ -0,0 +1,98 @@
+"use strict";
+
+const habilidades = document.querySelector(".habilidades__titulo");
+const servicios = document.querySelector(".servicios__titulo");
+const portafolio = document.querySelector(".proyectos__titulo");
+const menu = document.querySelector(".navbar__menu");
+const navbarActivado = document.querySelector(".navbar__activado");
+const bar = document.querySelector(".bar");
+const botonInicio = document.querySelector(".boton--inicio");
+const navBar = document.querySelector(".header");
+const logo = document.querySelector(".logo__contenedor")
+
+const verifyVisibility = (entradas) => {
+    for (const entrada of entradas) {
+        
+        if (entrada.isIntersecting) {
+            entrada.target.classList.add("animated");
+        }
+
+    }
+}
+
+
+const options = {
+    root: null,
+    rootMargin: "0px 0px -50px 0px",
+    threshold: 0.2
+}
+
+const observador = new IntersectionObserver(verifyVisibility,options);
+
+observador.observe(habilidades);
+observador.observe(servicios);
+observador.observe(portafolio);
+
+
+menu.addEventListener("click",()=> {
+    navbarActivado.classList.toggle("active");
+
+});
+
+menu.addEventListener("click",()=> {
+    bar.classList.toggle("animate");
+});
+
+document.addEventListener('click', (event) => {
+
+    const clicDentroDelMenu = menu.contains(event.target);
+    const clicDentroDelNavbarActivado = navbarActivado.contains(event.target);
+    if (!clicDentroDelMenu && !clicDentroDelNavbarActivado) {
+      navbarActivado.classList.remove('active');
+      bar.classList.remove("animate");
+    }
+});
+
+
+
+
+
+botonInicio.addEventListener("click", function() {
+    smoothScrollTo(0, 1000);
+});
+  
+function smoothScrollTo(targetPosition, duration) {
+    const startPosition = window.pageYOffset;
+    const distance = targetPosition - startPosition;
+    let startTime;
+  
+    function scroll(currentTime) {
+      if (!startTime) startTime = currentTime;
+      const elapsed = currentTime - startTime;
+      const easing = easeOutCubic(elapsed / duration);
+      const newPosition = startPosition + (distance * easing);
+      window.scrollTo(0, newPosition);
+      if (elapsed < duration) requestAnimationFrame(scroll);
+    }
+  
+    function easeOutCubic(t) {
+      return (t -= 1) * t * t + 1;
+    };
+  
+    requestAnimationFrame(scroll);
+  }
+  
+  
+
+
+window.addEventListener("scroll",()=> {
+    if (window.pageYOffset > 0) {
+        navBar.classList.add("scrolled");
+        logo.classList.add("logo-activo");
+    } 
+    
+    else {
+        navBar.classList.remove("scrolled");
+        logo.classList.remove("logo-activo");
+    }
+})
